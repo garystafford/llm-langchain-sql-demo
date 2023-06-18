e287cfa042a3a381a65d9140fabce6bf4f828f21
gary.a.stafford@gmail.com <gary.a.stafford@gmail.com>
2023-06-18 12:17:54 +0000

    Refactor Streamlit app, remove old version

---
+++ streamlit_demo/README.md
@@ -0,0 +1,33 @@
+# Natural Language Query (NLQ) Demo
+
+Natural Language Query (NLQ) Demo of Amazon Redshift using OpenAI's LLMs, LangChain, and Streamlit.
+
+Author: Gary A. Stafford
+
+Date: 2023-06-14
+
+License: MIT
+
+## Install Required Packages
+
+```sh
+# Required for ChromaDB with Amazon SageMaker Jumpstart
+sudo yum update -y && sudo yum install gcc gcc-c++ make -y
+
+# Older version required for sqlalchemy-redshift
+python3 -m pip install SQLAlchemy==1.4.48 -q
+
+python3 -m pip install langchain openai sqlalchemy-redshift -Uq
+python3 -m pip install streamlit streamlit-chat psycopg2-binary chromadb python-dotenv -Uq
+python3 -m pip install pyyaml -q
+python3 -m pip install sentence-transformers -Uq --no-cache-dir #--force-reinstall
+
+# Check package versions
+pip list | grep "langchain\|openai\|sentence-transformers\|SQLAlchemy"
+```
+
+## Start Applications
+
+```
+streamlit run app.py --server.runOnSave true
+```
--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -1,17 +1,7 @@
-# Natural Language Query Demo of Amazon Redshift using Streamlit and OpenAI
+# Natural Language Query (NLQ) Demo of Amazon Redshift using OpenAI's LLMs, LangChain, and Streamlit.
 # Author: Gary A. Stafford
-# Date: 2023-06-09
+# Date: 2023-06-14
 # License: MIT
-
-# sudo yum update -y && sudo yum install gcc gcc-c++ make
-# python3 -m pip install SQLAlchemy==1.4.48 -q # older version required for sqlalchemy-redshift
-# python3 -m pip install langchain openai sqlalchemy-redshift -Uq
-# python3 -m pip install streamlit streamlit-chat psycopg2-binary chromadb python-dotenv -Uq
-# python3 -m pip install pyyaml -q
-# python3 -m pip install sentence-transformers -Uq --no-cache-dir #--force-reinstall
-# pip list | grep "langchain\|openai\|sentence-transformers\|SQLAlchemy"
-
-# streamlit run app.py --server.runOnSave true
 
 import os
 
@@ -23,11 +13,9 @@ from langchain import (
     SQLDatabase,
     SQLDatabaseChain,
 )
-from langchain.chains import SQLDatabaseSequentialChain
 from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _postgres_prompt
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.llms import OpenAI
 from langchain.prompts.example_selector.semantic_similarity import (
     SemanticSimilarityExampleSelector,
 )
@@ -38,35 +26,47 @@ import streamlit as st
 
 
 def main():
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # load environment variables
     load_dotenv()
 
-    # llm = OpenAI(model_name="text-davinci-003", temperature=0, verbose=True)
+    # select llm to use
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, verbose=True)
+
+    # define datasource uri
     redshift_endpoint = get_datastore_credentials()
     db = SQLDatabase.from_uri(redshift_endpoint)
+
+    # load examples for few shot prmopting
     examples = load_samples()
 
-    st.set_page_config(
-        page_title="LangChain Natural Language Query Demo", page_icon=":robot:"
-    )
+    st.set_page_config(page_title="Natural Language Query (NLQ) Demo")
 
-    # chain = load_chain(llm, db)
     chain = load_few_shot_chain(llm, db, examples)
 
-    st.header("LangChain NLQ Demo")
-    st.subheader("Datasource")
-    st.text("Amazon Redshift TICKIT database")
-    st.subheader("Sample Questions")
-    st.text(
-        """
-    • How many venues are there in state of NY?
-    • How many customers made a purchase in May 2022?
-    • What were the total sales in September 2022?
-    • What are the largest 3 events based on all time gross sales?
-    • Who are the top 5 sellers based on all time gross sales?
-    • Which venue hosted the most events?
-    """
-    )
+    # Store the initial value of widgets in session state
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
+
+    # hide streamlit default features
+    # hide_menu_style = """
+    #     <style>
+    #     #MainMenu {visibility: hidden; }
+    #     footer {visibility: hidden;}
+    #     </style>
+    #     """
+    # st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+    # define streamlit colums
+    col1, col2 = st.columns([4, 1], gap="large")
+
+    # build the streamlit sidebar
+    build_sidebar()
+
+    # build the main app ui
+    build_form(col1, col2)
 
     if "generated" not in st.session_state:
         st.session_state["generated"] = []
@@ -74,13 +74,26 @@ def main():
     if "past" not in st.session_state:
         st.session_state["past"] = []
 
-    user_input = get_text()
+    if "query" not in st.session_state:
+        st.session_state["query"] = []
+
+    if "query_text" not in st.session_state:
+        st.session_state["query_text"] = []
+
+    # get the users query
+    get_text(col1)
+    user_input = st.session_state["query"]
 
     if user_input:
-        output = chain.run(query=user_input)
-
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
+        try:
+            output = chain.run(query=user_input)
+            st.session_state.past.append(user_input)
+            st.session_state.generated.append(output)
+        except Exception as exc:
+            st.session_state.past.append(user_input)
+            st.session_state.generated.append(
+                "The datasource does not contain information to answer this question."
+            )
 
     if st.session_state["generated"]:
         for i in range(len(st.session_state["generated"]) - 1, -1, -1):
@@ -146,18 +159,108 @@ def load_few_shot_chain(llm, db, examples):
     return db_chain
 
 
-def load_chain(llm, db):
-    db_chain = SQLDatabaseSequentialChain.from_llm(
-        llm, db, verbose=True, use_query_checker=False
-    )
-    return db_chain
+def get_text(col1):
+    with col1:
+        input_text = st.text_input(
+            "Ask a question:",
+            "",
+            key="query_text",
+            placeholder="Your question here...",
+            on_change=clear_text(),
+        )
 
 
-def get_text():
-    input_text = st.text_input(
-        "Question:", "", key="query", placeholder="Your question..."
-    )
-    return input_text
+def clear_text():
+    st.session_state["query"] = st.session_state["query_text"]
+    st.session_state["query_text"] = ""
+
+
+def build_sidebar():
+    with st.sidebar:
+        st.title("Technologies")
+
+        st.subheader("Natural Language Query (NLQ)")
+        st.write(
+            """
+        [Natural language query (NLQ)](https://www.yellowfinbi.com/glossary/natural-language-query) enables analytics users to ask questions of their data. It parses for keywords and generates relevant answers sourced from related databases, with results typically delivered as a report, chart or textual explanation that attempt to answer the query, and provide depth of understanding.
+        """
+        )
+
+        st.subheader("Amazon SageMaker Studio")
+        st.write(
+            """
+        [Amazon SageMaker Studio](https://aws.amazon.com/sagemaker/studio/) is a fully integrated development environment (IDE) where you can perform all machine learning (ML) development steps, from preparing data to building, training, and deploying your ML models, inclduing [JumpStart Foundation Models](https://docs.aws.amazon.com/sagemaker/latest/dg/jumpstart-foundation-models.html).
+        """
+        )
+
+        st.subheader("LangChain")
+        st.write(
+            """
+        [LangChain](https://python.langchain.com/en/latest/index.html) is a framework for developing applications powered by language models.
+        """
+        )
+
+        st.subheader("Chroma")
+        st.write(
+            """
+        [Chroma](https://www.trychroma.com/) is the open-source embedding database. Chroma makes it easy to build LLM apps by making knowledge, facts, and skills pluggable for LLMs.
+        """
+        )
+
+        st.subheader("Streamlit")
+        st.write(
+            """
+        [Streamlit](https://streamlit.io/) is an open-source app framework for Machine Learning and Data Science teams. Streamlit turns data scripts into shareable web apps in minutes. All in pure Python. No front-end experience required.
+        """
+        )
+
+        st.subheader("TICKIT Database")
+        st.write(
+            """
+        [TICKIT database](https://docs.aws.amazon.com/redshift/latest/dg/c_sampledb.html) is a small database consists of seven tables: two fact tables and five dimensions. This sample database track sales activity for the fictional TICKIT web site, where users buy and sell tickets online for sporting events, shows, and concerts.
+        """
+        )
+
+
+def build_form(col1, col2):
+    with col1:
+        with st.container():
+            st.title("Natural Language Query Demo")
+            st.subheader("Ask questions of your data using natural language.")
+
+        with st.container():
+            option = st.selectbox(
+                "Choose a datasource:",
+                (
+                    "Sales (Amazon Redshift)",
+                    "Inventory (Amazon RDS for PostgreSQL)",
+                    "Human Resources (Amazon Aurora MySQL)",
+                ),
+                label_visibility=st.session_state.visibility,
+                disabled=st.session_state.disabled,
+            )
+            with st.expander("Sample questions"):
+                st.text(
+                    """
+                How many venues are there in state of NY?
+                Which venue hosted the most events?
+                How many unique customers made a purchase in May 2022?
+                What were the total sales in September 2022?
+                What are the largest 3 events based on all time sales? How many tickets were sold to those events?
+                Who are the top 5 seller, based on all time sales?
+                Who are the top 3 seller, by names, based on all time sales? What were their sales?
+                What ingredients are in a chocolate cake?
+                Who is the current leader of Canada?
+                """
+                )
+    with col2:
+        with st.container():
+            st.button("clear chat", on_click=clear_session)
+
+
+def clear_session():
+    for key in st.session_state.keys():
+        del st.session_state[key]
 
 
 if __name__ == "__main__":
